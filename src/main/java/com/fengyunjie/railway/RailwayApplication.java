8fa0291ae7c57e105a16f021163c3e2b8dd68dfe
fengyunjie <759876936@qq.com>
2016-12-08 08:39:37 +0800

    fist init local code

---
+++ src/main/java/com/fengyunjie/railway/RailwayApplication.java
@@ -0,0 +1,12 @@
+package com.fengyunjie.railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayApplication.class, args);
+	}
+}
